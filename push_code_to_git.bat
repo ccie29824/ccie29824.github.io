bc950b11fc8c7ea7f1d5a79888aa5ce1749bf6cc
Rachit Gupta <rachit.gupta@live.com>
2020-08-17 02:51:34 +0530

    Update push_code_to_git.bat

--- push_code_to_git.bat
+++ push_code_to_git.bat
@@ -2,12 +2,12 @@
 git init
 git add .
 git commit -m "my page"
-git config --global user.email supratip.banerjee@yahoo.in
-git config --global user.email "Supratip Banerjee"
+git config --global user.email rachit.gupta@live.com
+git config --global user.email "Rachit Gupta"
 
 ::create the below <repo_name> in git
 ::name a new repo for local the below <repo_name> in git
-::git remote add <repo> https://github.com/supratipb/<repo_name>.git
+::git remote add <repo> https://github.com/ccie29824/<repo_name>.git
 
-git remote add cert https://github.com/supratipb/supratipb.github.io.git
+git remote add cert https://github.com/ccie29824/ccie29824.github.io.git
 git push cert master  
